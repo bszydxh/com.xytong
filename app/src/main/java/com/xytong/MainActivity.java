a1cd50eca75f34e179a70d426c9a3900a7db731c
bszydxh <2733879291hls@gmail.com>
2022-03-09 16:22:27 +0800

    阅读理解

--- app/src/main/java/com/xytong/MainActivity.java
+++ app/src/main/java/com/xytong/MainActivity.java
@@ -15,29 +15,28 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.xytong.databinding.ActivityMainBinding;
+import com.xytong.databinding.AppBarMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
-
+    private ActivityMainBinding binding;//为activity_main.xml绑定视图,先定义一个类,之后赋值
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//声明onCreate,方法继承之前的状态
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());//赋值阶段,inflate为调用生成的绑定类中包含的静态方法。这将为要使用的活动创建一个绑定类的实例。
+        setContentView(binding.getRoot());//binding中getRoot()方法是对binding根视图的引用
+        setSupportActionBar(binding.appBarMain.toolbar);//设置toolbar
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {//对右下方悬浮按键绑定监听事件
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view) {//@override注释后对编译器会对重写的方法进行检查
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)//snackbar类似于toast,它们在移动设备的屏幕底部和较大设备的左下方显示一条简短消息
+                        .setAction("Action", null).show();//设置点击事件
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        DrawerLayout drawer = binding.drawerLayout;//定义DrawerLayout变量drawer,将主视图的drawer赋值到该变量
+        NavigationView navigationView = binding.navView;//跟上面同理//nav就是drawer的一个子视图
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
