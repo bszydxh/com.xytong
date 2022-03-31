d1e12a1f75d30f8f0603813cff5f4a6e6c0ac7ef
bszydxh <2733879291hls@gmail.com>
2022-04-01 00:18:18 +0800

    修改登陆页面

---
+++ app/src/main/java/com/xytong/HttpClientToServer.java
@@ -0,0 +1,5 @@
+package com.xytong;
+
+public class HttpClientToServer {
+
+}
--- app/src/main/java/com/xytong/LoginActivity.java
+++ app/src/main/java/com/xytong/LoginActivity.java
@@ -13,7 +13,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        overridePendingTransition(R.anim.fragment_fade_enter,R.anim.fragment_fade_exit);
+        overridePendingTransition(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit);//进入渐变动画
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());//binding中getRoot()方法是对binding根视图的引用,也相当于创建视图
@@ -21,7 +21,20 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-                Toast.makeText(LoginActivity.this, "123", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //请求服务器得到结果
+
+                Toast.makeText(LoginActivity.this, "sign in", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "sign_up", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -29,6 +42,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void finish() {
         super.finish();
-        overridePendingTransition(R.anim.fragment_fade_enter,R.anim.fragment_fade_exit);
+        overridePendingTransition(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit);//退出渐变动画
     }
 }
